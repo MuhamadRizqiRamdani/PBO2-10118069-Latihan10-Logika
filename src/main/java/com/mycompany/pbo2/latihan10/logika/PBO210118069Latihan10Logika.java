1295cb4888da206a534dcc7049d8f1c0016b0de8
Muhamad Rizqi Ramdani <55569266+MuhamadRizqiRamdani@users.noreply.github.com>
2019-09-26 14:57:54 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/pbo2/latihan10/logika/PBO210118069Latihan10Logika.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pbo2.latihan10.logika;
+
+/**
+ *
+ * @author
+ * NAMA     : Muhamad Rizqi Ramdani
+ * KELAS    : PBO2
+ * NIM      : 10118069
+ * Deskripsi program : Program ini berisi program untuk menampilkan status dewasa
+ */
+public class PBO210118069Latihan10Logika {
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " +  dewasa);
+    }
+    
+}
